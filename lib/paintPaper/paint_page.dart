0c852fd1a259b606ba37172cf2e6021eed53099b
yang <restrive_14@163.com>
2024-03-09 11:13:24 +0800

    feat: 支持选择线条宽度

--- lib/paintPaper/paint_page.dart
+++ lib/paintPaper/paint_page.dart
@@ -1,6 +1,7 @@
 import 'package:demo/model/Line.dart';
 import 'package:demo/paintPaper/paper_appBar.dart';
 import 'package:demo/paintPaper/paper_painter.dart';
+import 'package:demo/paintPaper/storkWidthSelector.dart';
 import 'package:flutter/material.dart';
 
 class PaintPaper extends StatefulWidget {
@@ -17,7 +18,7 @@ class _PaintPaperState extends State<PaintPaper> {
   int _activeColorIndex = 0;
   // 线宽激活索引
   int _activeStorkWidthIndex = 0;
-
+  // 支持选择的颜色列表
   final List<Color> supportColors = [
     Colors.black,
     Colors.red,
@@ -28,37 +29,40 @@ class _PaintPaperState extends State<PaintPaper> {
     Colors.indigo,
     Colors.purple,
   ];
-
+  // 支持选择的线条宽度列表
   final List<double> supportStorkWidths = [1, 2, 4, 6, 8, 10];
 
   void _clear() {
     showDialog(
-        context: context,
-        builder: (ctx) => AlertDialog(
-              title: const Text('提示'),
-              content: const Text('是否清除画板?'),
-              actions: [
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(ctx).pop();
-                    },
-                    child: const Text('取消')),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(ctx).pop();
-                    setState(() {
-                      _lines.clear();
-                    });
-                  },
-                  child: const Text('确定'),
-                ),
-              ],
-            ));
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('提示'),
+        content: const Text('是否清除画板?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+            child: const Text('取消'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+              setState(() {
+                _lines.clear();
+              });
+            },
+            child: const Text('确定'),
+          ),
+        ],
+      ),
+    );
   }
 
   void _onPanStart(DragStartDetails details) {
     _lines.add(Line(
       points: [details.localPosition],
+      strokeWidth: supportStorkWidths[_activeStorkWidthIndex],
     ));
   }
 
@@ -67,19 +71,41 @@ class _PaintPaperState extends State<PaintPaper> {
     setState(() {});
   }
 
+  void _onSelectStorkWidth(int index) {
+    if (index != _activeStorkWidthIndex) {
+      setState(() {
+        _activeStorkWidthIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PaintPaperAppBar(onClear: _clear),
-      body: GestureDetector(
-        onPanStart: _onPanStart,
-        onPanUpdate: _onPanUpdate,
-        child: CustomPaint(
-          painter: PaperPainter(lines: _lines),
-          child: ConstrainedBox(
-            constraints: const BoxConstraints.expand(),
+      body: Stack(
+        children: [
+          GestureDetector(
+            onPanStart: _onPanStart,
+            onPanUpdate: _onPanUpdate,
+            child: CustomPaint(
+              painter: PaperPainter(lines: _lines),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints.expand(),
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 0,
+            right: 10,
+            child: StorkWidthSelector(
+              supportStorkWidths: supportStorkWidths,
+              activeIndex: _activeStorkWidthIndex,
+              color: Colors.black,
+              onSelect: _onSelectStorkWidth,
+            ),
+          ),
+        ],
       ),
     );
   }
